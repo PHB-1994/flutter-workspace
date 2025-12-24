51414a4f6ef8f1a94f7445c60624574ff0e378bd
PHB-1994 <gudqls0991@naver.com>
2025-12-24 14:23:28 +0900

    void function if for

--- lib/basic/for_screen.dart
+++ lib/basic/for_screen.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class ForScreen extends StatelessWidget {
+class ForScreen extends StatefulWidget {
   const ForScreen({super.key});
 
+  @override
+  State<ForScreen> createState() => _ForScreen();
+}
+
+class _ForScreen extends State<ForScreen> {
   // appbar 이용해서 메인으로 가는 버튼 생성
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("for 문 예제"),
-        backgroundColor: Colors.blue,
+        title: const Text("for 반복문 확인하기"),
+        backgroundColor: Colors.yellow,
+        // 돌아가기 아이콘 추가
         leading: IconButton(
-            onPressed: () => context.go('/'),
-            icon: const Icon(Icons.arrow_back)),
+          onPressed: () => context.go('/'),
+          icon: const Icon(Icons.arrow_back),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('점수 : ', style: const TextStyle(fontSize: 24)),
+            Text('반복문 화면'),
             ElevatedButton(
-                onPressed: () => context.go('/'),
-                child: const Text("등급확인"))
+              onPressed: () => context.go('/'),
+              child: const Text("등급확인"),
+            ),
           ],
         ),
       ),
